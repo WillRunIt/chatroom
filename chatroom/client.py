4e04f5ad385e434742dc23d789402c77f837a3b5
Willem De Kerpel <dekerpelwillem@hotmail.com>
2024-06-08 13:17:36 +0300

    Add username save function

--- chatroom/client.py
+++ chatroom/client.py
@@ -1,11 +1,23 @@
+import json
 import socket
 import pickle
+import os
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('localhost', 65432)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
+
+data_directory = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.join(data_directory, 'userinfo.json')
+if not os.path.exists(data_path):
+    with open(data_path, 'w') as data_file:
+        username = input('Please enter your username: ')
+        json.dump({'username': username}, data_file)
+else:
+    with open(data_path, 'r') as data_file:
+        username = json.load(data_file)['username']
 
 message = input(">>>")
 binary_msg = pickle.dumps(message)
