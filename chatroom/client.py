191452e9125423750a0fd842f906e13e9dd6d737
Olha Vazhka <you@example.com>
2024-06-08 11:30:53 +0200

    Add server and client endpoints

---
+++ chatroom/client.py
@@ -0,0 +1,27 @@
+import socket
+import pickle
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('localhost', 65432)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+message = input(">>>")
+binary_msg = pickle.dumps(message)
+
+try:
+    print('sending {!r}'.format(*server_address))
+    sock.sendall(binary_msg)
+
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(1024)
+        amount_received += len(data)
+        print('received {!r}'.format(pickle.loads(data)))
+
+finally:
+    print('closing socket')
+    sock.close()
