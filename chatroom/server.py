3c92067e248d81b6d831ef26d8726b3d09fc5330
Your Name <you@example.com>
2024-06-08 14:37:16 +0300

    Add server update

--- chatroom/server.py
+++ chatroom/server.py
@@ -1,27 +1,22 @@
-import socket
 import pickle
+import threading
+import socketserver
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 65432)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+HOST = "localhost"
+PORT = 65432
 
-sock.listen(1)
-
-while True:
-    print('waiting for connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from ', client_address)
-        while True:
-            data = connection.recv(1024)
+messages_queue = []
+message_queue_lock = threading.Lock()
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        global messages_queue
+        try:
+            data = self.request.recv(1024)
             data = pickle.loads(data)
-            print('received {!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall(pickle.dumps(data))
-            else:
-                print('no data from', client_address)
-                break
-    finally:
-        connection.close()
+            self.request.sendall(pickle.dumps(data))
+        except pickle.UnpicklingError:
+            print("Keepalive received")
+
+if __name__ == "__main__":
+    print("Server started")
+    server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
